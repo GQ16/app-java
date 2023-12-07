86eb424c2cb58348b74159e740f5ce10030ca304
GQ16 <107271596+GQ16@users.noreply.github.com>
2023-12-06 21:33:25 -0600

    Add files via upload

---
+++ src/main/java/neoflix/services/FavoriteService.java
@@ -0,0 +1,151 @@
+package neoflix.services;
+
+import neoflix.AppUtils;
+import neoflix.Params;
+import org.neo4j.driver.Driver;
+import neoflix.ValidationException;
+import org.neo4j.driver.exceptions.ClientException;
+import org.neo4j.driver.exceptions.NoSuchRecordException;
+import org.neo4j.driver.Values;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FavoriteService {
+    private final Driver driver;
+
+    private final List<Map<String,Object>> popular;
+    private final List<Map<String,Object>> users;
+    private final Map<String,List<Map<String,Object>>> userFavorites = new HashMap<>();
+
+    /**
+     * The constructor expects an instance of the Neo4j Driver, which will be
+     * used to interact with Neo4j.
+     *
+     * @param driver
+     */
+    public FavoriteService(Driver driver) {
+        this.driver = driver;
+        this.popular = AppUtils.loadFixtureList("popular");
+        this.users = AppUtils.loadFixtureList("users");
+    }
+
+    /**
+     * This method should retrieve a list of movies that have an incoming :HAS_FAVORITE
+     * relationship from a User node with the supplied `userId`.
+     *
+     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * in the `order` parameter.
+     * Results should be limited to the number passed as `limit`.
+     * The `skip` variable should be used to skip a certain number of rows.
+     *
+     * @param userId  The unique ID of the user
+     * @param params Query params for pagination and sorting
+     * @return List<Movie> An list of Movie objects
+     */
+    // tag::all[]
+    public List<Map<String, Object>> all(String userId, Params params) {
+        // Open a new session
+        try (var session = this.driver.session()) {
+            // Retrieve a list of movies favorited by the user
+            var favorites = session.executeRead(tx -> {
+                String query = String.format("""
+                            MATCH (u:User {userId: $userId})-[r:HAS_FAVORITE]->(m:Movie)
+                            RETURN m {
+                            .*,
+                                favorite: true
+                            } AS movie
+                            ORDER BY m.`%s` %s
+                            SKIP $skip
+                            LIMIT $limit
+                        """, params.sort(Params.Sort.title), params.order());
+                var res = tx.run(query, Values.parameters("userId", userId, "skip", params.skip(), "limit", params.limit()));
+                return res.list(row -> row.get("movie").asMap());
+            });
+            return favorites;
+        }
+    }
+    // end::all[]
+
+    /**
+     * This method should create a `:HAS_FAVORITE` relationship between
+     * the User and Movie ID nodes provided.
+     *
+     * If either the user or movie cannot be found, a `NotFoundError` should be thrown.
+     *
+     * @param userId The unique ID for the User node
+     * @param movieId The unique tmdbId for the Movie node
+     * @return Map<String,Object></String,Object> The updated movie record with `favorite` set to true
+     */
+    // tag::add[]
+    public Map<String,Object> add(String userId, String movieId) {
+        // Open a new Session
+        try (var session = this.driver.session()) {
+            // Create HAS_FAVORITE relationship within a Write Transaction
+            var favorite = session.executeWrite(tx -> {
+                String statement = """
+                            MATCH (u:User {userId: $userId})
+                            MATCH (m:Movie {tmdbId: $movieId})
+
+                            MERGE (u)-[r:HAS_FAVORITE]->(m)
+                                    ON CREATE SET r.createdAt = datetime()
+
+                            RETURN m {
+                                .*,
+                                favorite: true
+                            } AS movie
+                        """;
+                var res = tx.run(statement, Values.parameters("userId", userId, "movieId", movieId));
+                // return res.single().get("movie").asMap();
+                return res.single().get("movie").asMap();
+            });
+
+            // Return movie details and `favorite` property
+            return favorite;
+        // Throw an error if the user or movie could not be found
+        } catch (NoSuchRecordException e) {
+            throw new ValidationException(String.format("Couldn't create a favorite relationship for User %s and Movie %s", userId, movieId), Map.of("movie",movieId, "user",userId));
+        }
+    }
+    // end::add[]
+
+    /*
+     *This method should remove the `:HAS_FAVORITE` relationship between
+     * the User and Movie ID nodes provided.
+     * If either the user, movie or the relationship between them cannot be found,
+     * a `NotFoundError` should be thrown.
+
+     * @param userId The unique ID for the User node
+     * @param movieId The unique tmdbId for the Movie node
+     * @return Map<String,Object></String,Object> The updated movie record with `favorite` set to true
+     */
+    // tag::remove[]
+    public Map<String,Object> remove(String userId, String movieId) {
+        // Open a new Session
+        try (var session = this.driver.session()) {
+            // Removes HAS_FAVORITE relationship within a Write Transaction
+            var favorite = session.executeWrite(tx -> {
+                String statement = """
+                          MATCH (u:User {userId: $userId})-[r:HAS_FAVORITE]->(m:Movie {tmdbId: $movieId})
+                          DELETE r
+
+                          RETURN m {
+                            .*,
+                            favorite: false
+                          } AS movie
+                        """;
+                var res = tx.run(statement, Values.parameters("userId", userId, "movieId", movieId));
+                return res.single().get("movie").asMap();
+            });
+
+            // Return movie details and `favorite` property
+            return favorite;
+        // Throw an error if the user or movie could not be found
+        } catch (NoSuchRecordException e) {
+            throw new ValidationException("Could not remove favorite movie for user", Map.of("movie",movieId, "user",userId));
+        }
+    }
+    // end::remove[]
+
+}
---
+++ src/main/java/neoflix/services/RatingService.java
@@ -0,0 +1,93 @@
+package neoflix.services;
+
+import neoflix.AppUtils;
+import neoflix.Params;
+import neoflix.ValidationException;
+
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.exceptions.ClientException;
+import org.neo4j.driver.exceptions.NoSuchRecordException;
+import org.neo4j.driver.Values;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RatingService {
+    private final Driver driver;
+    private final List<Map<String, Object>> ratings;
+    private final Map<String, Object> pulpfiction;
+
+    /**
+     * The constructor expects an instance of the Neo4j Driver, which will be
+     * used to interact with Neo4j.
+     */
+    public RatingService(Driver driver) {
+        this.driver = driver;
+        this.ratings = AppUtils.loadFixtureList("ratings");
+        this.pulpfiction = AppUtils.loadFixtureSingle("pulpfiction");
+    }
+
+    /**
+     * Return a paginated list of reviews for a Movie.
+     *
+     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * in the `order` parameter.
+     * Results should be limited to the number passed as `limit`.
+     * The `skip` variable should be used to skip a certain number of rows.
+     *
+     * @param {string} id       The tmdbId for the movie
+     * @param {string} sort  The field to order the results by
+     * @param {string} order    The direction of the order (ASC/DESC)
+     * @param {number} limit    The total number of records to return
+     * @param {number} skip     The number of records to skip
+     * @returns {Promise<Record<string, any>>}
+     */
+    // tag::forMovie[]
+    public List<Map<String,Object>> forMovie(String id, Params params) {
+        // TODO: Get ratings for a Movie
+
+        return AppUtils.process(ratings,params);
+    }
+    // end::forMovie[]
+
+
+    /**
+     * Add a relationship between a User and Movie with a `rating` property.
+     * The `rating` parameter should be converted to a Neo4j Integer.
+     *
+     * If the User or Movie cannot be found, a NotFoundError should be thrown
+     *
+     * @param {string} userId   the userId for the user
+     * @param {string} movieId  The tmdbId for the Movie
+     * @param {number} rating   An integer representing the rating from 1-5
+     * @returns {Promise<Record<string, any>>}  A movie object with a rating property appended
+     */
+    public Map<String,Object> add(String userId, String movieId, int rating) {
+        // Save the rating in the database
+
+        // Open a new session
+        try (var session = this.driver.session()) {
+
+            // Run the cypher query
+            var movie = session.executeWrite(tx -> {
+                String query = """
+                        MATCH (u:User {userId: $userId})
+                        MATCH (m:Movie {tmdbId: $movieId})
+
+                        MERGE (u)-[r:RATED]->(m)
+                        SET r.rating = $rating, r.timestamp = timestamp()
+
+                        RETURN m { .*, rating: r.rating } AS movie
+                        """;
+                var res = tx.run(query, Values.parameters("userId", userId, "movieId", movieId, "rating", rating));
+                return res.single().get("movie").asMap();
+            });
+
+            // Return movie details with rating
+            return movie;
+        } catch(NoSuchRecordException e) {
+            throw new ValidationException("Movie or user not found to add rating", Map.of("movie", movieId, "user", userId));
+        }
+    }
+}
